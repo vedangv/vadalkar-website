5463121743b3567e63750e6c60102d25706dc285
Vedang <vedangv@stanford.edu>
2026-03-25 18:20:07 -0700

    chore: remove nested blog repository from tracking

--- blog/src/sanity/lib/image.ts
+++
@@ -1,8 +0,0 @@
-import createImageUrlBuilder from '@sanity/image-url'
-import { client } from './client'
-
-const builder = createImageUrlBuilder(client)
-
-export function urlFor(source: any) {
-  return builder.image(source)
-}
--- blog/src/sanity/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-import post from './post'
-import category from './category'
-import author from './author'
-
-export const schemaTypes = [post, category, author]
